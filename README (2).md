f12699c1aa33cb46509c6df81d4f136c34af3949
Zoli Bajan <ivanyimozes@gmail.com>
2018-07-16 13:03:45 +0200

    07-16 aktuális állás..befejezetlen

--- README (2).md
+++ README (2).md
@@ -1,21 +1,34 @@
-4. Írasd ki így kapott hajók adatait.
 
-5. Készítened kell egy statisztikát, mely kiírja a következő statisztikai adatokat: 
+
+### Feladatok
+
+1. A kapott adatokat rendezd ár(cost_in_credits) szerint növekvő sorrendbe. (advanced bubble)
+
+2. Töröld az összes olyan adatot (tehát az objektumot a tömbből), ahol a consumables értéke NULL. Fontos, hogy ne csak undefined-ra állítsd a tömbelemet!!!
+
+3. Az összes NULL értéket (minden objektum minden tulajdonságánál) módosítsd "unknown"-ra
+
+4. A shapceship-list class-ű divbe jelenítsd meg az így kapott hajók adatait, beleérve a képét is.
+
+5. Ha valamelyik hajó adatait tartalmazó html elemre (pl.: a divre amibe benne van minden adata) rákattintunk,
+akkor töltse be az adott hajó adatait a one-spaceship class-ű div-be.
+
+6. Készítened kell egy statisztikát, mely a shapceship-list class-ű div aljára a következő adatokat fogja beleírni: 
 
 * Egy fős (crew = 1) legénységgel rendelkező hajók darabszáma.
 * A legnagyobb cargo_capacity-vel rendelkező hajó neve (model)
 * Az összes hajó utasainak (passengers) összesített száma
 * A leghosszabb(lengthiness) hajó képének a neve
 
-6. Legyen lehetőség a hajókra rákeresni _model_ szerint. (logaritmikus/binary sort)
+7. Legyen lehetőség a hajókra rákeresni _model_ szerint.
 
 * A keresett nevet paraméterként kapja a függvényed.
 * A keresés nem case sensitive
 * Nem csak teljes egyezést vizsgálunk, tehát ha a keresett szöveg szerepel a hajó nevében már az is találat
 * Ha több találatunk is lenne, azt a hajót adjuk vissza, amelyiknek a neve ABC sorrendben a legelső lenne.
-* Írasd ki a hajó adatait.
+* Az adott hajó adatait a one-spaceship class-ű div-be kell megjeleníteni rendezett formában, képpel együtt.
 
 ## Git
-Az ekészült munkádat tedd fel egy git repo-ba githubra. A repo neve az alábbi formátum szerint legyen megadva: vezeteknev-keresztnev-basicexam.
+Az ekészült munkádat tedd fel egy git repo-ba githubra. A repo neve az alábbi formátum szerint legyen megadva: vezeteknev-keresztnev-starwarsgyak.
 Értelemszerűen a saját vezeték és keresztneved add meg.
 
--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // ide deklaráljátok a függvényeket.
 
 
-//1. A kapott adatokat rendezd ár(cost_in_credits) szerint növekvő sorrendbe. (advanced bubble)
+//1. A kapott adatokat rendezd ár(cost_in_credits) szerint növekvő sorrendbe.
 
 function sortByPriceAscending(input) {
   var i = input.length - 1;
@@ -9,7 +9,7 @@ function sortByPriceAscending(input) {
   while (i > 0) {
     csere = 0;
     for (var j = 0; j < i; j++) {
-      if (parseInt(input[j].cost_in_credits) > parseInt(input[j + 1].cost_in_credits)) {
+      if ((input[j].cost_in_credits == "unknown") || (parseInt(input[j].cost_in_credits) > parseInt(input[j + 1].cost_in_credits))) {
         [input[j], input[j + 1]] = [input[j + 1], input[j]];
         csere = j;
       }
@@ -17,26 +17,24 @@ function sortByPriceAscending(input) {
     i = csere;
   }
 }
-//nem teszteltem, de a facebook csoportban talált kód alapján így kéne kinéznie
 
 //2. Töröld az összes olyan adatot (tehát az objektumot a tömbből) ahol a consumables értéke NULL.
 
 function delNullConsumables(input) {
-  for (var i = 0; i < input.length; i++) {
-    if (input[i].consumables === null) {
+  for (var i = input.length - 1; i >= 0; i--) {
+    if (input[i].consumables == null) {
       input.splice(i, 1)
     }
   }
 }
 // Array.splice(x,1); --remove element at position x
-//input tömb tetszőleges indexű elemének eltávolítására ezt a megoldást találtam
 
 //3. Az összes NULL értéket (minden objektum minden tulajdonságánál) módosítsd "unknown"-ra
 
 function replaceNullValuesToUnknown(input) {
   for (var i = 0; i < input.length; i++) {
     for (var j in input[i]) {
-      if (input[i].hasOwnProperty(k)) {
+      if (input[i].hasOwnProperty(j)) {
         if (input[i][j] === null) {
           input[i][j] = 'unknown';
         }
@@ -46,12 +44,110 @@ function replaceNullValuesToUnknown(input) {
 }
 
 //https://stackoverflow.com/questions/9907419/how-to-get-a-key-in-a-javascript-object-by-its-value
-//162. válasz!!!
 
 
 //4. Írasd ki így kapott hajók adatait.
 
 
+
+function displayData(input) {
+  var target = document.querySelector('.spaceship-list');
+
+  for (var i = 0; i < input.length; i++) {
+    var div = document.createElement('div');
+    div.className = 'oneShip';
+    var result = "";
+    for (var j in input[i]) {
+      result += j + ' : ' + input[i][j] + '<br>';
+    }
+    result += "<br><hr>";
+    div.innerHTML = result;
+    target.appendChild(div);
+  }
+
+}
+
+//6. Készítened kell egy statisztikát, mely kiírja a következő statisztikai adatokat:
+
+function statistics(input) {
+  var displayStats = `Egy fős legénységű hajók száma: ${statCrew(input)} db<br>
+  A legnagyobb rakterű hajó neve: ${statCargo(input)}<br>
+  Az összes utas száma: ${statPassengers(input)}<br>
+  A leghosszabb hajó képe: ${statImgOfLongest(input)}`;
+  var target = document.querySelector('.spaceship-list');
+  var div = document.createElement('div');
+  div.className = "statistics";
+  div.innerHTML = displayStats;
+  target.appendChild(div);
+}
+
+//Egy fős (crew = 1) legénységgel rendelkező hajók darabszáma.
+function statCrew(input) {
+  var counter = 0;
+  for (var i = input.length - 1; i >= 0; i--) {
+    if (input[i].crew == "1") {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+//A legnagyobb cargo_capacity-vel rendelkező hajó neve (model)
+function statCargo(input) {
+  var name = "";
+  var maxCapacity = parseInt(input[0].cargo_capacity);
+  for (var i = 0; i < input.length; i++) {
+    if (parseInt(input[i].cargo_capacity) > maxCapacity) {
+      maxCapacity = parseInt(input[i].cargo_capacity);
+      name = input[i].model;
+    }
+  }
+  return name + ':' + maxCapacity;
+}
+
+//Az összes hajó utasainak (passengers) összesített száma
+function statPassengers(input) {
+  var sumPassengers = 0;
+  for (var i = 0; i < input.length; i++) {
+    if (isNaN(parseInt(input[i].passengers)) == false) {
+      sumPassengers += parseInt(input[i].passengers)
+    }
+  }
+  return sumPassengers
+}
+
+//A leghosszabb(lengthiness) hajó képének a neve
+function statImgOfLongest(input) {
+  var img = "";
+  var maxLength = parseInt(input[0].lengthiness);
+  for (var i = 0; i < input.length; i++) {
+    if (parseInt(input[i].lengthiness) > maxLength) {
+      maxLength = parseInt(input[i].lengthiness);
+      img = input[i].image;
+    }
+  }
+  return img;
+}
+
+/*7. Legyen lehetőség a hajókra rákeresni _model_ szerint. (logaritmikus/binary sort)
+
+* A keresett nevet paraméterként kapja a függvényed.
+* A keresés nem case sensitive
+* Nem csak teljes egyezést vizsgálunk, tehát ha a keresett szöveg szerepel a hajó nevében már az is találat
+* Ha több találatunk is lenne, azt a hajót adjuk vissza, amelyiknek a neve ABC sorrendben a legelső lenne.
+* Írasd ki a hajó adatait.*/
+
+function search(input, find) {
+  var result = null;
+  for (var i = input.length - 1; i >= 0; i--) {
+    if (input[i].model.toLowerCase().indexOf(find) > -1) {
+      if ((result == null) || (input[i].model < result.model)) {
+        result = input[i];
+      }
+    }
+  }
+  return result;
+}
 
 function getData(url, callbackFunc) {
   var xhttp = new XMLHttpRequest();
@@ -68,8 +164,11 @@ function successAjax(xhttp) {
   // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen lehet hívni.
-  sortByPriceAscending(userDatas);
+
   delNullConsumables(userDatas);
   replaceNullValuesToUnknown(userDatas);
+  sortByPriceAscending(userDatas);
+  displayData(userDatas);
+  statistics(userDatas);
 }
 getData('/json/spaceships.json', successAjax);
